985b1e206840ce5114d5913dd84688486ae39839
Santiago <sfgomez1998@gmail.com>
2020-07-24 11:14:00 +0300

    added tasks to Shaders folder

---
+++ Shaders/Uniforms/build/CMakeFiles/Uniforms.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "../Uniforms"
+  "../Uniforms.pdb"
+  "CMakeFiles/Uniforms.dir/src/glad.c.o"
+  "CMakeFiles/Uniforms.dir/src/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/Uniforms.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
